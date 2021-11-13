bee81019c0dc46953f5f18586e211fcb89f656b5
Victor Castañeda <vidanilo10@gmail.com>
2021-11-13 16:31:18 -0500

    Completed profile feed.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from rest_framework.decorators import authentication_classes
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, filters
@@ -5,6 +6,7 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
 from users import serializers, models, permissions
 
@@ -18,3 +20,12 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
 class UserLoginApiView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.UpdateOwnProfile, IsAuthenticated)
+
+    def perfom_create(self, serializer):
+        serializer.save(user_profile=self.request.user)
