c5494688d841e64f3ccd2876d3b4e8fa588347a3
Victor Castañeda <vidanilo10@gmail.com>
2021-11-13 15:47:31 -0500

    Adding permissions

--- users/views.py
+++ users/views.py
@@ -2,9 +2,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
-from users import serializers, models
+from users import serializers, models, permissions
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
